c2a164592cbceec83b0fba9136d5bcf303ebbb3c
Ronnie Balonon Jr. <ronniebalonon1996@gmail.com>
2026-05-23 22:56:09 +0400

    Add CV PDF and fix outdated link in PromoPopup

--- src/components/PromoPopup.tsx
+++ src/components/PromoPopup.tsx
@@ -78,7 +78,7 @@ const PromoPopup = () => {
                     asChild
                     onClick={handleClose}
                   >
-                    <a href="/Alvin_Jampazar_CV.pdf" download className="flex items-center justify-center gap-2">
+                    <a href="/Ronnie_Balonon_CV.pdf" download className="flex items-center justify-center gap-2">
                       <Download className="w-4 h-4" />
                       Download CV
                     </a>
